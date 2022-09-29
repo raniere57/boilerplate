8b14d25c6feaf2a90c69b1ee67df8edccc19918e
raniere57 <raaniere.r@gmail.com>
2022-09-29 16:43:05 -0300

    configurado integração do eslint-prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-
 export default function Home({ title = 'React Avançado' }) {
-
-
-  const tal = 'bla'
-
   return (
     <div className={styles.container}>
       <Head>
